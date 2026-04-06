89659e1c326ac9a6583a852b717624cb9d4ea4c2
Saira485 <sayaligangurde48@gmail.com>
2026-04-06 16:34:37 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 using namespace std;
 
+// Recursive function
 int factorial(int n) {
     if(n == 0 || n == 1)
-        return 1;
-    return n * factorial(n-1);
+        return 1;   // base case
+    else
+        return n * factorial(n - 1);  // recursive call
 }
 
 int main() {
     int n;
+
+    cout << "Enter a number: ";
     cin >> n;
-    cout << factorial(n);
+
+    int result = factorial(n);
+
+    cout << "Factorial of " << n << " is: " << result;
+
+    return 0;
 }
