68ccfa43b02bdbd62946145886a7b9c83d3112a3
Saira485 <sayaligangurde48@gmail.com>
2026-04-06 19:52:56 +0530

    Update Count comparision in bubble sort.cpp

--- Count comparision in bubble sort.cpp
+++ Count comparision in bubble sort.cpp
@@ -2,19 +2,40 @@
 using namespace std;
 
 int main() {
-    int n, count = 0;
+    int n;
+
+    cout << "Enter number of elements: ";
     cin >> n;
+
     int arr[n];
 
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-i-1; j++) {
-            count++;
-            if(arr[j] > arr[j+1])
-                swap(arr[j], arr[j+1]);
+    int comparisons = 0;
+
+    // Bubble Sort
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            comparisons++;  // count comparison
+
+            if(arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
 
-    cout << "Comparisons: " << count;
+    cout << "\nSorted array:\n";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\nTotal comparisons: " << comparisons;
+
+    return 0;
 }
