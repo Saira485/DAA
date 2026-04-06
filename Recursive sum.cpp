d333cf23b5759f968906bc55662204c609794114
Saira485 <sayaligangurde48@gmail.com>
2026-04-06 19:18:07 +0530

    Update Recursive sum.cpp

--- Recursive sum.cpp
+++ Recursive sum.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int sum(int n) {
+// Recursive function
+int sum(int arr[], int n) {
     if(n == 0)
-        return 0;
-    return n + sum(n-1);
+        return 0;   // base case
+    else
+        return arr[n - 1] + sum(arr, n - 1);  // recursive call
 }
 
 int main() {
     int n;
+
+    cout << "Enter number of elements: ";
     cin >> n;
-    cout << sum(n);
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int total = sum(arr, n);
+
+    cout << "Total sum = " << total;
+
+    return 0;
 }
